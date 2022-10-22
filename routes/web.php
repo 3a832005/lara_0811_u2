f7fb683093fa455fbda31d688444bb2dd5d9d3ce
Li Yen Tzu <3a832005@gm.student.ncut.edu.tw>
2022-10-22 23:34:43 +0800

    練習2:修改參數成選擇性

--- routes/web.php
+++ routes/web.php
@@ -34,3 +34,7 @@ Route::get('r2',function (){
 Route::get('hello/{name}',function ($name){
     return 'Hello, '.$name;
 });
+
+Route::get('hello/{name?}',function ($name='Everybody'){
+    return'Hello, '.$name;
+});
