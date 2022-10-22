99a0618bc960b51bc25d241de0c161c6f3f65460
Li Yen Tzu <3a832005@gm.student.ncut.edu.tw>
2022-10-22 23:59:07 +0800

    練習4:為Route命名

--- routes/web.php
+++ routes/web.php
@@ -13,16 +13,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-/*
+/*練習1:設定Route回傳View
 Route::get('/',function (){
     return view('welcome')
 });
 */
 
+//練習1:設定Route回傳字串
 Route::get('/',function (){
     return 'welcome';
 });
 
+//練習1:設定Route跳轉路由
 Route::get('r1',function (){
     return redirect('r2');
 });
@@ -31,14 +33,23 @@ Route::get('r2',function (){
     return view('welcome');
 });
 
+//練習2:設定Route接受參數
 Route::get('hello/{name}',function ($name){
     return 'Hello, '.$name;
 });
 
+/*練習2:修改參數成選擇性
 Route::get('hello/{name?}',function ($name='Everybody'){
     return'Hello, '.$name;
 });
+*/
 
+//練習3:測試artisan指令新增route
 Route::get('welcome/{name}',function ($name){
     return 'welcome, '.$name;
 });
+
+//練習4:為Route命名
+Route::get('hello/{name?}',function ($name='Everybody'){
+    return'Hello, '.$name;
+})->name('hello.index');
