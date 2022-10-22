4c297b7e82f05f764241b53e189b62c6c0e6ee04
Li Yen Tzu <3a832005@gm.student.ncut.edu.tw>
2022-10-22 23:19:14 +0800

    練習1:設定Route跳轉路由

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/*
+Route::get('/',function (){
+    return view('welcome')
+});
+*/
 
 Route::get('/',function (){
     return 'welcome';
 });
 
+Route::get('r1',function (){
+    return redirect('r2');
+});
+
+Route::get('r2',function (){
+    return view('welcome');
+});
